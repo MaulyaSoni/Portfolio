135b74bd0974320ff303aca1e3a9c5ce6f5ed3f3
MaulyaSoni <24maulya@gmail.com>
2025-08-24 19:31:45 +0530

    deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 export default defineConfig({
-  base: '/Portfolio',
+  base: '/Portfolio/',
   plugins: [react()],
   build: {
     outDir: 'build'  // <-- maybe dist is renamed to 'build' or something else
